4cc71cf52d08e5f4ef9d0962ddff7651597d5287
Konovalov Alexander <akonovalov@technoserv.com>
2016-02-13 06:29:47 +0300

    refactor variable names

--- public/js/main.js
+++ public/js/main.js
@@ -2,16 +2,16 @@ $(document).ready(function () {
 
     $('#button-generate-pass').click(function() {
         console.log('button-generate-pass clicked:');
-        var new_pass = generate_new_pass();
-        $('#output-generated-pass').text(new_pass);
+        var newPass = generate_new_pass();
+        $('#output-generated-pass').text(newPass);
     });
 
     // copy to clipboard button implementation
     var client = new ZeroClipboard($("#button-copy-pass"));
     client.on( "copy", function (event) {
         var clipboard = event.clipboardData;
-        var text_to_copy = $('#output-generated-pass').text();
-        clipboard.setData( "text/plain", text_to_copy );
+        var textToCopy = $('#output-generated-pass').text();
+        clipboard.setData( "text/plain", textToCopy );
         var successCopy = "Пароль успешно скопирован в буфер обмена";
         var outputMessage = $('#output-message');
         outputMessage.removeClass();
@@ -31,63 +31,63 @@ $(document).ready(function () {
 
 function generate_new_pass() {
 
-    var new_pass = "";
+    var newPass = "";
 
     // get password length
-    var pass_length_text = $('#input-number').val();
-    var pass_length = parseInt(pass_length_text);
-    if (isNaN(pass_length)) {
+    var passLengthText = $('#input-number').val();
+    var passLength = parseInt(passLengthText);
+    if (isNaN(passLength)) {
         alert('Password length should be an integer value!');
         return "";
     }
 
-    var numbers_set = ["0","1","2","3","4","5","6","7","8","9"];
+    var numbersSet = ["0","1","2","3","4","5","6","7","8","9"];
 
-    var lower_letters_set =
+    var lowerLetterSet =
         [
             "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
         ];
-    var upper_letters_set =
+    var upperLetterSet =
         [
             "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
             "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
         ];
-    var special_set =
+    var specialSet =
         [
           "@", "#", "$", "%", "^", "&", "*"
         ];
 
     // make array of symbol sets
-    var charset_power = 0;
-    var array_of_sets = [];
+    var charsetPower = 0;
+    var arrayOfSets = [];
     $('.symbol-set').each(function() {
        if ($(this).hasClass('active')) {
            if ($(this).hasClass('numbers')) {
-               array_of_sets.push(numbers_set);
-               charset_power += numbers_set.length;
+               arrayOfSets.push(numbersSet);
+               charsetPower += numbersSet.length;
            } else if ($(this).hasClass('upper-char')) {
-               array_of_sets.push(upper_letters_set);
-               charset_power += upper_letters_set.length;
+               arrayOfSets.push(upperLetterSet);
+               charsetPower += upperLetterSet.length;
            } else if ($(this).hasClass('lower-char')) {
-               array_of_sets.push(lower_letters_set);
-               charset_power += lower_letters_set.length;
+               arrayOfSets.push(lowerLetterSet);
+               charsetPower += lowerLetterSet.length;
            } else if ($(this).hasClass('special-symbols')) {
-               array_of_sets.push(special_set);
-               charset_power += special_set.length;
+               arrayOfSets.push(specialSet);
+               charsetPower += specialSet.length;
            }
        }
     });
 
     // generate pass from symbol sets
-    for (var i = 0; i < pass_length; i++) {
-        var array_of_symbols = get_random_item_from_array(array_of_sets);
-        var rand_symbol = get_random_item_from_array(array_of_symbols);
-        new_pass += rand_symbol;
+    for (var i = 0; i < passLength; i++) {
+        var arrayOfSymbols = get_random_item_from_array(arrayOfSets);
+        var randSymbol = get_random_item_from_array(arrayOfSymbols);
+        newPass += randSymbol;
     }
 
     // set password strength
-    var bits = pass_length * Math.round((Math.log(charset_power) / Math.log(2)) + 0.5);
+    var bits = passLength * Math.round((Math.log(charsetPower) / Math.log(2)) + 0.5);
     //console.log("charset_power = " + charset_power);
     //console.log("bits = " + bits);
     if (bits < 48) {
@@ -100,14 +100,14 @@ function generate_new_pass() {
         set_pass_color_message_pic('very-strong', 'сверхнадежный', 'img/smile-happy.svg')
     }
 
-        return new_pass;
+        return newPass;
 }
 
 function get_random_item_from_array(array) {
     var len = array.length;
-    var rand_item_index = Math.round(Math.random() * (len - 1));
-    var rand_item = array[rand_item_index];
-    return rand_item;
+    var randItemIndex = Math.round(Math.random() * (len - 1));
+    var randItem = array[randItemIndex];
+    return randItem;
 }
 
 function set_pass_color_message_pic(strength, message, pic) {
