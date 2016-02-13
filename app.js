b2656355cc16cd98d83128001160b59edb752cdf
fr019 <fr019@yandex.ru>
2016-02-13 22:46:42 +0300

    add page 404 and delete /users url

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -30,16 +29,25 @@ app.use(require('node-compass')({
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
+    res.render('404', {
+        title: 'Good Password | Страница не найдена'
+    });
     next(err);
+});
+
+// cache settings
+app.use(function (req, res, next) {
+    if (!res.getHeader('Cache-Control'))
+        res.setHeader('Cache-Control',
+            'public, max-age=' + (maxAge / 1000));
 });
 
 // error handlers
