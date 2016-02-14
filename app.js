96e90bdc990b8c11b532db3657adeefe61a7cceb
fr019 <fr019@yandex.ru>
2016-02-14 15:24:18 +0300

    add favicon

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(path.join(__dirname, 'public', 'img/favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', '/img/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
