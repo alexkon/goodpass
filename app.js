a9d0e6317674a05af176919ff00efc0a60b1dcd7
fr019 <fr019@yandex.ru>
2016-03-20 12:34:28 +0300

    add gzip compression

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var compress = require('compression');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -12,6 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(compress());
 
 app.use(favicon(path.join(__dirname, 'public', '/img/favicon.ico')));
 app.use(logger('dev'));
