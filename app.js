d96e16f42fc0d44b3cd957ece992db09441d294e
fr019 <fr019@yandex.ru>
2016-05-11 20:57:24 +0300

    remove bower dependencies

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(require('node-compass')({
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 app.use('/', routes);
 
