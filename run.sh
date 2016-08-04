7c6e606026bedb0d9b4c23644126219fb75e0016
Konovalov Alexander <aleksaander@ya.ru>
2016-08-04 10:32:10 +0300

    add redirect port 80 to 3000 to run as a simple user

--- run.sh
+++ run.sh
@@ -2,3 +2,6 @@
 
 export DEBUG=goodpass:*
 nohup node bin/www > logs/main.log 2>&1 &
+
+# Redirect port 80 to port 3000 to run program from simple user
+#sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 3000
